c48fbc0a2a4b067dc244e2b8c23d861818512140
Ben H <deepfreespace@gmail.com>
2021-09-19 16:43:14 -0600

    Rearranged while loop to break immediately if player chooses to skip.

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,28 +6,36 @@ var enemyNames = ["Roborto","Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-console.log(enemyNames);
-console.log(enemyNames.length);
-console.log(enemyNames[0]);
-console.log(enemyNames[3]);
-
 var fight = function(enemyName) {
     // Repeat the fight as long as the enemy robot is alive
-    while(enemyHealth > 0) {
+    while(playerHealth > 0 && enemyHealth > 0) {
         // Ask player if they want to fight or skip the fight
         var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
-        // If player chooses to fight, run the fight sequence
-        if (promptFight === "fight" || promptFight === "FIGHT") {
+        if (promptFight === "skip" || promptFight === "SKIP") {
+            // Confirm player wants to skip
+            var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+            // If player skips, leave the fight and lose money
+            if (confirmSkip) {
+                window.alert(playerName + " has decided to skip this fight. Goodbye!");
+                // Subtract money from player for skipping
+                playerMoney = playerMoney - 2;
+                console.log("Player Money = ", playerMoney);
+                break;
+            // If the player doesn't skip, run fight again
+            }
+            // If player chooses to fight, run the fight sequence
+        } else if (promptFight === "fight" || promptFight === "FIGHT") {
             // Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
             enemyHealth = enemyHealth - playerAttack;
             // Log a resulting message to the console so we know that it worked
             console.log(
                 playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
             );
-            // Check enemy's health
-            if (enemyHealth <=0) {
+            // Check enemy's health; if <=0, exit the fight loop
+            if (enemyHealth <= 0) {
                 window.alert(enemyName + " has died!");
+                break;
             } else {
                 window.alert(enemyName + " still has " + enemyHealth + " health left.");
             }
@@ -37,31 +45,20 @@ var fight = function(enemyName) {
             console.log(
                 enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
             );
-            // Check player's health
+            // Check player's health; if <=0, exit the fight loop
             if (playerHealth <= 0) {
                 window.alert(playerName + " has died!");
+                break;
             } else {
                 window.alert(playerName + " still has " + playerHealth + " health left.");
             }
             
-        // If player chooses to skip, confirm; if yes, leave fight, and subtract money; if no, run fight again
-        } else if (promptFight === "skip" || promptFight === "SKIP") {
-            // Confirm player wants to skip
-            var confirmSkip = window.confirm("Are you sure you'd like to quit?");
-            // If player skips, leave the fight and lose money
-            if (confirmSkip) {
-                window.alert(playerName + " has decided to skip this fight. Goodbye!");
-                // Subtract money from player for skipping
-                playerMoney = playerMoney - 2;
-            // If the player doesn't skip, run fight again
-            } else {
-                fight();
-            }
         } else {
             window.alert("You need to choose a valid option. Try Again!");
         }
     }
 }
+
 // Fight through each enemy in the enemyNames array
 for (var i = 0; i < enemyNames.length; i++) {
     // debugger;
