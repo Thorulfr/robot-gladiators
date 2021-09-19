c96275467aecbcad7da2390dbcf5e60d9beebeb1
Ben H <deepfreespace@gmail.com>
2021-09-18 22:27:22 -0600

    Testing out JS functionalities.

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,8 @@
-window.alert("This is an alert! JavaScript is running!");
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// Prompt the user to enter their robot's name
+var playerName = window.prompt("What is your robot's name?");
+console.log("Our robot's name is " + playerName + " .");
+// fight();
